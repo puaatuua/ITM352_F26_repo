06352387d9b56374855dbab09e56b0d8342bcfef
Ava Puaatuua <puaatuua@hawaii.edu>
2026-09-02 13:12:32 -1000

    Exercises 3 & 4

---
+++ Lab2/Ex3.py
@@ -0,0 +1,12 @@
+# Ask the user to enter a decimal number, calculate the square of that number,
+# and print it out.
+
+# Name: Ava Puaatuua
+# Date: September 2, 2026
+
+input_value = input("Enter a floating point number: ")
+float_value = float(input_value)
+squared_value = float_value ** 2
+
+print("You entered: ", float_value)
+print("The square of the number you entered is: ", squared_value)
