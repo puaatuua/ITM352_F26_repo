858fee6cba1a2d891c711a47840be1a2b7e8822c
Ava Puaatuua <puaatuua@hawaii.edu>
2026-09-08 11:11:39 -1000

    Added comments for Lab3 exercises

--- Lab3/Ex1.py
+++ Lab3/Ex1.py
@@ -1,3 +1,10 @@
+# This is a simple Python script to check the version of the cryptography library installed in your environment.
+
+# Name: Ava Puaatuua
+# Date: September 8, 2026
+
+# Import the cryptography library
 import cryptography
 
+# Check and print the version of the cryptography library
 print(cryptography.__version__)
--- Lab3/Ex1b.py
+++ Lab3/Ex1b.py
@@ -1,9 +1,22 @@
+# This is a simple Python script to demonstrate the use of the cryptography library for 
+# symmetric encryption and decryption using the Fernet module.
+
+# Name: Ava Puaatuua
+# Date: September 8, 2026
+
+# Import the excryption class
+# Fernet is a symmetric encryption tool from the cryptography library
 from cryptography.fernet import Fernet
 
+# Generate a unique secret key for encryption and decryption
 key = Fernet.generate_key()
+# Create a cipher object using the generated key
 cipher_suite = Fernet(key)
 
+# Encrypts the string "Hello, World!" and prints the encoded text
 encoded_text = cipher_suite.encrypt(b"Hello, World!")
 print("Encoded_text: ", encoded_text)
+
+# Reverse the encryption
 decoded_text = cipher_suite.decrypt(encoded_text)
 print("Decoded_text: ", decoded_text)
