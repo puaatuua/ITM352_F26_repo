ac9dc8780195c8d05aff82ff09f4012f5f08f4d2
Ava Puaatuua <puaatuua@hawaii.edu>
2026-09-09 13:21:00 -1000

    Create Ex2a.py

---
+++ Lab3/Ex2a.py
@@ -0,0 +1,14 @@
+# This script calculates the midpoint between two numbers.
+
+# Name: Ava Puaatuua
+# Date: September 8, 2026
+
+def midpoint(number1, number2):
+    return (number1 + number2) / 2
+
+'''number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+result = midpoint(number1, number2)
+print(f"The midpoint between {number1} and {number2} is: {result}")'''
+
+print(midpoint(10, 20))  # Example usage
