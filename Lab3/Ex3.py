c7d9e7b039739b8feff303c2628acc68e5e9088f
Ava Puaatuua <puaatuua@hawaii.edu>
2026-09-10 17:47:58 -1000

    Create Ex3.py

---
+++ Lab3/Ex3.py
@@ -0,0 +1,9 @@
+# This program takes a number and returns the squareroot of that number
+
+# Name: Ava Puaatuua
+# Date: September 10, 2026
+
+def squareroot(number):
+	return number ** 0.5
+
+print(squareroot(16))  # Example usage
