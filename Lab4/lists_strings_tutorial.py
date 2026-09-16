d7dad5a3b919ac7e6e157b77c7675e6d910b5737
Ava Puaatuua <puaatuua@hawaii.edu>
2026-09-15 21:14:19 -1000

    Create lists_strings_tutorial.py

---
+++ Lab4/lists_strings_tutorial.py
@@ -0,0 +1,8 @@
+favorite_movies = ["Spider-Man", "Inception", "The Matrix", "Interstellar", "The Dark Knight"] # List of five favorite movies
+favorite_movies.append("The Lord of the Rings")  # Adding a new movie to the list
+favorite_movies.insert(2, "Pulp Fiction")  # Inserting a movie at index 2
+favorite_movies.remove("Inception")  # Removing a movie from the list
+favorite_movies.pop()  # Removing the last movie from the list
+favorite_movies.sort()  # Sorting the list of favorite movies in alphabetical order
+favorite_movies.reverse()  # Reversing the order of the list
+print(favorite_movies)  # Display the updated list of favorite movies
